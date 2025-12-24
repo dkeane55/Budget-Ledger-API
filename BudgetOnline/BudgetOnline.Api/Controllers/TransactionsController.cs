6f0e3ab19c707edeeed9d7a46165e5658c3ce86d
Dave <davidkeane55@gmail.com>
2025-12-24 11:41:48 +1100

    feat: add TransactionsController with skeleton endpoints

---
+++ BudgetOnline/BudgetOnline.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,32 @@
+﻿using BudgetOnline.Domain.Entities;
+using BudgetOnline.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetOnline.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TransactionsController : ControllerBase    
+{
+    private readonly ApplicationDbContext _context;
+
+    public TransactionsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions() {
+        return await _context.Transactions.ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Transaction>> CreateTransaction(Transaction transaction)
+    {
+        _context.Transactions.Add(transaction);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetTransactions), new { id = transaction.Id }, transaction);
+    }
+}
--- BudgetOnline/BudgetOnline.Api/Program.cs
+++ BudgetOnline/BudgetOnline.Api/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -19,7 +20,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.MapControllers();
 
 
 
